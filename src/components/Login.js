ef862ea284b9978eab4a8196507af1c3140a31d8
Keegan <keeganfinegan@gmail.com>
2020-12-03 12:22:53 +1000

    new

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,116 +1,27 @@
 import React from 'react';
-import {
-  Paper,
-  withStyles,
-  Grid,
-  TextField,
-  Button,
-  FormControlLabel,
-  Checkbox,
-} from '@material-ui/core';
-import { Face, Fingerprint } from '@material-ui/icons';
-const styles = (theme) => ({
-  margin: {
-    margin: theme.spacing.unit * 2,
-  },
-  padding: {
-    padding: theme.spacing.unit,
-  },
-});
+import ReactDOM from 'react-dom';
 
-class Login extends React.Component {
-  render() {
-    const { classes } = this.props;
+// or
+import { GoogleLogin } from 'react-google-login';
 
-    // Post and validate user login data to the REST API
-    function HandlePost(email, password, setResponse) {
-      const url = 'http://localhost:1337/grades';
+const responseGoogle = (response) => {
+  console.log(response);
+};
 
-      return fetch(url, {
-        method: 'POST',
-        headers: {
-          accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email: email, password: password }),
-      })
-        .then((res) => res.json())
-        .then((response) => {
-          localStorage.setItem('token', response.token);
-          setResponse(response);
-        })
-        .catch((error) => {
-          console.log(error);
-          setResponse(error);
-        });
-    }
-    return (
-      <div className="login">
-        <p>LOGIN</p>
-        <Paper className={classes.padding}>
-          <div className={classes.margin}>
-            <Grid container spacing={8} alignItems="flex-end">
-              <Grid item>
-                <Face />
-              </Grid>
-              <Grid item md={true} sm={true} xs={true}>
-                <TextField
-                  id="username"
-                  label="Username"
-                  type="email"
-                  fullWidth
-                  autoFocus
-                  required
-                />
-              </Grid>
-            </Grid>
-            <Grid container spacing={8} alignItems="flex-end">
-              <Grid item>
-                <Fingerprint />
-              </Grid>
-              <Grid item md={true} sm={true} xs={true}>
-                <TextField
-                  id="username"
-                  label="Password"
-                  type="password"
-                  fullWidth
-                  required
-                />
-              </Grid>
-            </Grid>
-            <Grid container alignItems="center" justify="space-between">
-              <Grid item>
-                <FormControlLabel
-                  control={<Checkbox color="#000000" />}
-                  label="Remember me"
-                />
-              </Grid>
-              <Grid item>
-                <Button
-                  disableFocusRipple
-                  disableRipple
-                  style={{ textTransform: 'none' }}
-                  variant="text"
-                  color="#000000"
-                >
-                  Forgot password ?
-                </Button>
-              </Grid>
-            </Grid>
-            <Grid container justify="center" style={{ marginTop: '10px' }}>
-              <Button
-                variant="outlined"
-                color="#000000"
-                style={{ textTransform: 'none' }}
-              >
-                Login
-              </Button>
-            </Grid>
-          </div>
-        </Paper>
-      </div>
-    );
-  }
-}
+const Login = () => {
+  return (
+    <div className="login">
+      <GoogleLogin
+        clientId="
+        62126108860-l9eai98p2qspo0amkhbbsu9e7puhar5e.apps.googleusercontent.com
+        "
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
+    </div>
+  );
+};
 
-export default withStyles(styles)(Login);
+export default Login;

c3f67c6a1c1108b65f8a8c686261d9fc75034a29
Keegan <keeganfinegan@gmail.com>
2020-12-03 11:20:27 +1000

    more changes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import InputTable from './components/InputTable';
-import TableEditablePage from './components/Table';
-import Home from './components/Home';
 import Drawer from './components/Drawer';
 import Login from './components/Login';
 import { BrowserRouter } from 'react-router-dom';
@@ -15,7 +13,7 @@ function App() {
         <Drawer />
         <Switch>
           <Route exact path="/" component={InputTable} />
-          <Route exact path="/Login" component={Login} />
+          <Route path="/Login" component={Login} />
         </Switch>
       </div>
     </BrowserRouter>
